ef62103588ae9f67e5f6ab113b17418d7af3668d
computerkunst <71874970+computerkunst@users.noreply.github.com>
2020-10-02 11:05:00 +0200

    Update sketch.js

--- Kunstzeicheneditor/sketch.js
+++ Kunstzeicheneditor/sketch.js
@@ -1,11 +1,10 @@
 // Was geht hier vor: Wie ist der Code des Kunstzeicheneditors aufgebaut?
 // CC JLK 2020
-
 // Was wird alles benötigt
-let colorPicker;	// greift auf das Farbpaletten Fenster von javascript zu
-let slider;		// greift auf das slidertool von javascript zu
-var jepp = 0;
+let colorPicker;  // greift auf das Farbpaletten Fenster von javascript zu
+let slider;       // greift auf das slidertool von javascript zu
 
+var jepp = 0;
 function setup() {
   // put setup code here
   
@@ -13,24 +12,25 @@ function setup() {
  // createCanvas(windowWidth-10, windowHeight-10);
   createCanvas(874, 620);// oder Postkartengröße
 
-  button = createButton('drück mich');		// erstellt einen drückbaren Knopf mit der Beschriftung "drück mich"
-  button.position(10, 20);			// an dieser Position
-  button.mousePressed(HintergrundRandom);	// hinterlegt den Funktionsaufrug wenn der Button geklickt wird
+ button = createButton('drück mich');   // erstellt einen drückbaren Knopf mit der Beschriftung "drück mich"
+  button.position(10, 20);      // an dieser Position
+  button.mousePressed(HintergrundRandom); // hinterlegt den Funktionsaufrug wenn der Button geklickt wird
 
-  slider = createSlider(0, 3, 2);		// erstellt den Slider von min 0 bis max 3 und voreingestellt auf 2
+  slider = createSlider(0, 3, 2);   // erstellt den Slider von min 0 bis max 3 und voreingestellt auf 2
   slider.position(20, 50);
-  slider.style('width', '80px');		// definiert die Breite des Sliders
+  slider.style('width', '80px');    // definiert die Breite des Sliders
 
-  slider1 = createSlider(-100, 400, 20);	// erstellt den zweiten Slider von min -100 bis max 400 und voreingestellt auf Wert 20
+  slider1 = createSlider(-100, 400, 20);  // erstellt den zweiten Slider von min -100 bis max 400 und voreingestellt auf Wert 20
   slider1.position(20, 80);
   slider1.style('width', '80px');
 
-  colorPicker = createColorPicker('#000000');	// erstellt den ColorPicker mit Schwarz als voreingestellte Farbe
-  colorPicker.position(25,110);			// an dieser Position
+  colorPicker = createColorPicker('#000000'); // erstellt den ColorPicker mit Schwarz als voreingestellte Farbe
+  colorPicker.position(25,110);     // an dieser Position
 
-  button1 = createButton('speichern');		// noch ein Tastfeld
+  button1 = createButton('speichern');    // noch ein Tastfeld
   button1.position(15, 140);
-  button1.mousePressed(Aufruf);			// wenn geklickt folgt es dem Aufruf siehe unten
+  button1.mousePressed(Aufruf);     // wenn geklickt folgt es dem Aufruf siehe unten
+
 
 
   textSize(20);
@@ -82,16 +82,15 @@ else{
 
 
 //////////////////////////////77
-
 function HintergrundRandom() {
-	
+  
   jepp = jepp + 1;
   if(jepp > 1){
   let r = random(255);
   let g = random(255);
   let b = random(255);
   background(r,g,b);
-	}
+  }
   else  background(0);
 }
 
