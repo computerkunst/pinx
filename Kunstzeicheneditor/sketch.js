b847e0812481ddf02964944e89bf9529dccfa631
computerkunst <71874970+computerkunst@users.noreply.github.com>
2020-10-02 11:33:12 +0200

    Update sketch.js

--- Kunstzeicheneditor/sketch.js
+++ Kunstzeicheneditor/sketch.js
@@ -25,7 +25,7 @@ function setup() {
   slider1.style('width', '80px');
 
   colorPicker = createColorPicker('#000000'); // erstellt den ColorPicker mit Schwarz als voreingestellte Farbe
-  colorPicker.position(25,110);     // an dieser Position
+  colorPicker.position(25,110);     		// an dieser Position
 
   button1 = createButton('speichern');    // noch ein Tastfeld
   button1.position(15, 140);
@@ -35,55 +35,51 @@ function setup() {
 
   textSize(20);
   text('das Menü links ermöglicht dir verschiedene Farben auszuwählen, \ndein Werk zu speichern und verschiedene Linien Varianten zu verwenden!', 100, 100, width-100, height-100);
-}
-  
-  
+} 
 
 function draw() {
   // put drawing code here
-//background(0,100,200);
 let val = slider.value();
 let val1 = slider1.value();
-if(mouseIsPressed && val == 0)
-{
-  line(mouseX,mouseY,(width/2),(height/2));
-  stroke(colorPicker.color());
-  print(val1);
-
-}
-if(mouseIsPressed && val == 1)
-{
-  line(movedX,mouseY,mouseX,movedY);
-  stroke(colorPicker.color());
-  let m = map(val1, -100, 400, 1, 6);
-  strokeWeight(m);
-  print(val1);
-}
-
-if(mouseIsPressed && val == 2)
-{
-	line(mouseX,mouseY,mouseX-80,mouseY-180);
-	stroke(colorPicker.color());
-
-}
-
-if(mouseIsPressed && val == 3)
-{
-	line(mouseX-random(-100,val1),mouseY-random(-100,val1),mouseX-random(-100,val1),mouseY-random(-100, val));
-	stroke(colorPicker.color());
-  print(val1);
-
-}
-else{
 	
+//////////////////////// 4 Verschiedene Linien Programme werden jenachdem wie der Wert von slider.value ist eingestellt und aufgerufen
+if(mouseIsPressed && val == 0)		/// Programm 0
+	{
+  line(mouseX,mouseY,(width/2),(height/2));	// eine linie wird gezeichnet mit Startpunkt mouseX,Y Koordinaten und Endpunkt der halben Höhe und Breite = Zentrum in der Mitte
+  stroke(colorPicker.color());			// Farbe wird bestimmt
+  print(val1);					// Kontrollausgabe in Konsole
+	}
+if(mouseIsPressed && val == 1)		//// Programm 1
+	{
+  line(movedX,mouseY,mouseX,movedY);		// eine linie wird gezeichnet mit unterschiedlichen Variablen von mouseX,Y bzw. movedX und movedY
+  stroke(colorPicker.color());			// Farbe wird bestimmt
+  let m = map(val1, -100, 400, 1, 6);		// die Dicke der Linie kann hier bestimmt werden und wirkt sich auf alle anderen Programme aus!
+  strokeWeight(m);				// hier wird die strokeWeight also die Liniendicke angewendet Variable m
+  print(val1);
 }
+
+if(mouseIsPressed && val == 2)		//// Programm 2
+{
+ let laenge = map(val1, -100,400, 0,Heigth)
+   line(mouseX,mouseY,mouseX-laenge,mouseY-laenge);	// am simpelsten einfach nur eine Schräge Linie im Raum mit vordefinierter Länge
+   stroke(colorPicker.color());
+	
+
+}
+
+if(mouseIsPressed && val == 3)		//// Programm 3
+	{			// das letzte Programm definiert die Linienposition innerhalb einen gewissen Größenrahmens selbst.
+  line(mouseX-random(-100,val1),mouseY-random(-100,val1),mouseX-random(-100,val1),mouseY-random(-100, val));
+  stroke(colorPicker.color());
+  print(val1);
+
+	}
 
 }
 
 
 //////////////////////////////77
-function HintergrundRandom() {
-  
+function HintergrundRandom() {		// hier wird durch den Button "drück mich" die zufällige Hintergrundfarbe gewählt. Allerdings erzeugt der erste Klick die Farbe weiß
   jepp = jepp + 1;
   if(jepp > 1){
   let r = random(255);
@@ -91,14 +87,15 @@ function HintergrundRandom() {
   let b = random(255);
   background(r,g,b);
   }
-  else  background(0);
+  
+  else  {
+	  createCanvas(874,620);
+	  background(255,255,255,0);
 }
 
 function Aufruf(){
-	
-	//img = createImage(10, 10);
-	save('Komputerkunst.png');
-	//save('NeuesKunstwerk.jpg');
+  save('Komputerkunst.png');		// als .png Bild speichern
+  //save('NeuesKunstwerk.jpg');		// als .jpg Bild ginge natürlich auch
 }
 
 /* // nur aktivieren wenn Resizing erwünscht ist zum Beispiel bei Smartphonenutzung
